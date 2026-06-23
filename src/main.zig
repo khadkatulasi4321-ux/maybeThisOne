34b7c35acba6ac3cf5f2498b05ee0e853989f6bb
samsit-phew <samsitkarki@gmail.com>
2026-06-23 20:04:36 +0545

    ERR

--- src/main.zig
+++ src/main.zig
@@ -1,71 +1,79 @@
 const std = @import("std");
-const Io = std.Io;
-
-const maybeThisOne = @import("maybeThisOne");
+const hsr = @import("maybeThislib");
 
 pub fn main(init: std.process.Init) !void {
-    // Prints to stderr, unbuffered, ignoring potential errors.
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
-    // This is appropriate for anything that lives as long as the process.
-    const arena: std.mem.Allocator = init.arena.allocator();
-
-    // Accessing command line arguments:
-    const args = try init.minimal.args.toSlice(arena);
-    for (args) |arg| {
-        std.log.info("arg: {s}", .{arg});
+    const arena = init.arena;
+    const alloc = arena.allocator();
+    const args = try init.minimal.args.toSlice(alloc);
+    if (args.len != 5) {
+        std.debug.print(
+            "usage: {s} <sha256|blake3|sha512|sha1|md5> <hash> <wordlist> <bytesallocatedforwordlist>\n",
+            .{args[0]},
+        );
+        std.process.exit(1);
+    }
+    const opt = args[1];
+    const hash = args[2];
+    const equal = std.ascii.eqlIgnoreCase;
+    var sizeOfFileBuff = try std.fmt.parseInt(u16, args[4], 10);
+    if (sizeOfFileBuff > 8192) {
+        sizeOfFileBuff = 8192;
     }
 
-    // In order to do I/O operations need an `Io` instance.
-    const io = init.io;
-
-    // Stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    var stdout_buffer: [1024]u8 = undefined;
-    var stdout_file_writer: Io.File.Writer = .init(.stdout(), io, &stdout_buffer);
-    const stdout_writer = &stdout_file_writer.interface;
-
-    try maybeThisOne.printAnotherMessage(stdout_writer);
-
-    try stdout_writer.flush(); // Don't forget to flush!
-}
-
-test "simple test" {
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(i32) = .empty;
-    defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(gpa, 42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "fuzz example" {
-    try std.testing.fuzz({}, testOne, .{});
-}
-
-fn testOne(context: void, smith: *std.testing.Smith) !void {
-    _ = context;
-    // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(u8) = .empty;
-    defer list.deinit(gpa);
-    while (!smith.eos()) switch (smith.value(enum { add_data, dup_data })) {
-        .add_data => {
-            const slice = try list.addManyAsSlice(gpa, smith.value(u4));
-            smith.bytes(slice);
-        },
-        .dup_data => {
-            if (list.items.len == 0) continue;
-            if (list.items.len > std.math.maxInt(u32)) return error.SkipZigTest;
-            const len = smith.valueRangeAtMost(u32, 1, @min(32, list.items.len));
-            const off = smith.valueRangeAtMost(u32, 0, @intCast(list.items.len - len));
-            try list.appendSlice(gpa, list.items[off..][0..len]);
-            try std.testing.expectEqualSlices(
-                u8,
-                list.items[off..][0..len],
-                list.items[list.items.len - len ..],
+    const hashr: hsr.Hash =
+        if (equal(opt, "sha256"))
+            .sha256
+        else if (equal(opt, "blake3"))
+            .blake3
+        else if (equal(opt, "sha512"))
+            .sha512
+        else if (equal(opt, "md5"))
+            .md5
+        else if (equal(opt, "sha1"))
+            .sha1
+        else {
+            std.debug.print(
+                "unknown algorithm: {s}\n",
+                .{opt},
             );
-        },
-    };
+            std.process.exit(1);
+        };
+    if (hash.len != hashr.digestSize() * 2) {
+        std.debug.print("invalid hash for algorithm {s}", .{opt});
+        std.process.exit(1);
+    }
+    const gpa = init.gpa;
+    const fileBuff = try gpa.alloc(u8, sizeOfFileBuff);
+
+    const file = try std.Io.Dir.cwd().openFile(init.io, args[3], .{});
+    defer file.close(init.io);
+
+    var reader = file.reader(init.io, fileBuff);
+
+    var guesses: std.ArrayList([]u8) = .empty;
+
+    while (try reader.interface.takeDelimiter('\n')) |line| {
+        const owned = try init.arena.allocator().dupe(u8, line);
+        try guesses.append(gpa, owned);
+    }
+    gpa.free(fileBuff);
+
+    const hequal = std.mem.eql;
+
+    for (guesses.items) |guess| {
+        var digest: [64]u8 = undefined;
+        const digestx = try hashr.hashDigest(
+            guess,
+            digest[0..],
+        );
+        const s = hashr.digestSize();
+
+        if (hequal([]u8, digest[0..s], hash)) {
+            std.debug.print("hash found its hash of {x:0>2}", .{digestx});
+            std.process.exit(0);
+        }
+    }
+    std.debug.print("didnt find it ", .{});
+
+    guesses.deinit(gpa);
 }
--- src/root.zig
+++ src/root.zig
@@ -1,18 +1,103 @@
-//! By convention, root.zig is the root source file when making a package.
 const std = @import("std");
-const Io = std.Io;
+const sha256 = std.crypto.hash.sha2.Sha256;
+const blake3 = std.crypto.hash.Blake3;
+const sha1 = std.crypto.hash.Sha1;
+const sha512 = std.crypto.hash.sha2.Sha512;
+const md5 = std.crypto.hash.Md5;
 
-/// This is a documentation comment to explain the `printAnotherMessage` function below.
-///
-/// Accepting an `Io.Writer` instance is a handy way to write reusable code.
-pub fn printAnotherMessage(writer: *Io.Writer) Io.Writer.Error!void {
-    try writer.print("Run `zig build test` to run the tests.\n", .{});
-}
+// pub const hash = enum {
+//     sha256,
+//     sha512,
+//     blake3,
+//     pub fn hshdigest(self: hash, str: []const u8) ![32]u8 {
+//         switch (self) {
+//             .sha256 => {
+//                 var digest: [32]u8 = undefined;
+//                 sha256.hash(str, &digest[0..32], .{});
+//                 return digest;
+//             },
+//             .sha512 => {
+//                 var digest: [64]u8 = undefined;
+//                 sha512.hash(str, &digest, .{});
+//                 return digest;
+//             },
+//             .blake3 => {
+//                 var digest: [32]u8 = undefined;
+//                 blake3.hash(str, &digest, .{});
+//                 return digest;
+//             },
+//         }
+//     }
+// };
+pub const Hash = enum {
+    sha256,
+    sha512,
+    blake3,
+    sha1,
+    md5,
 
-pub fn add(a: i32, b: i32) i32 {
-    return a + b;
-}
+    pub fn digestSize(self: Hash) usize {
+        return switch (self) {
+            .sha256 => 32,
+            .sha512 => 64,
+            .blake3 => 32,
+            .md5 => 16,
+            .sha1 => 20,
+        };
+    }
 
-test "basic add functionality" {
-    try std.testing.expect(add(3, 7) == 10);
-}
+    pub fn hashDigest(
+        self: Hash,
+        input: []const u8,
+        out: []u8,
+    ) ![]const u8 {
+        const size = self.digestSize();
+
+        if (out.len < size)
+            return error.BufferTooSmall;
+
+        switch (self) {
+            .sha256 => {
+                std.crypto.hash.sha2.Sha256.hash(
+                    input,
+                    out[0..32],
+                    .{},
+                );
+            },
+
+            .sha512 => {
+                std.crypto.hash.sha2.Sha512.hash(
+                    input,
+                    out[0..64],
+                    .{},
+                );
+            },
+
+            .blake3 => {
+                std.crypto.hash.Blake3.hash(
+                    input,
+                    out[0..32],
+                    .{},
+                );
+            },
+
+            .md5 => {
+                std.crypto.hash.Md5.hash(
+                    input,
+                    out[0..16],
+                    .{},
+                );
+            },
+
+            .sha1 => {
+                std.crypto.hash.Sha1.hash(
+                    input,
+                    out[0..20],
+                    .{},
+                );
+            },
+        }
+
+        return out[0..size];
+    }
+};
